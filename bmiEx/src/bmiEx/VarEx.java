a5a3f147fea691752df3f2d21a88196d05761157
subinnote <subin1932@gmail.com>
2023-03-17 11:40:12 +0900

    test

---
+++ bmiEx/src/bmiEx/VarEx.java
@@ -0,0 +1,15 @@
+package bmiEx;
+
+public class VarEx {
+	public static void main(String[] args) {
+	
+		int value = 0;
+		// == 비교 연산자
+		// value값이 0이면 코드블럭을 실행
+		if(value == 0) {
+			int value2 = value + 20;
+		}
+		int sum = value;
+		
+	}
+}
